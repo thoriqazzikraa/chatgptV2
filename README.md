fc86b91fe13c0b227273084b8886181f9cba97b6
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-07-21 14:48:47 +0700

    add generateV2, this function don't need an apikey

--- README.md
+++ README.md
@@ -7,32 +7,37 @@ Scraping Chat Gpt from [chat.ramxn.dev](https://chat.ramxn.dev/chat/)
 # Usage
 
 ```
-(async () => {
-const chatgpt = require("@nechlophomeriaa/chatgptv2")
-const ai = await chatgpt.generate(text, model, apikey) // Default model is set to claude-instant-100k
-console.log(ai)
-})
+const { model, modelV2, generate, generateV2 } = require("@nechlophomeriaa/chatgptv2")
 ```
 
 # Apikey
 
 You can get the apikey for free by join [this discord server](https://discord.gg/chimeragpt)
 
-
 # Models
 
 ```
-const chatgpt = require("@nechlophomeriaa/chatgptv2")
-const model = chatgpt.model()
+const model = model()
 console.log(model)
+
+const model2 = modelV2()
+console.log(model2)
 ```
 
 # Example
 
 ```
 (async () => {
-const chatgpt = require("@nechlophomeriaa/chatgptv2")
-const ai = await chatgpt.generate("example javascript array", "claude-instant-100k")
+const ai = generate(text, model, apikey) // Default model is claude-instant-100k
+console.log(ai)
+})
+```
+
+Or you can use free version
+
+```
+(async () => {
+const ai = generateV2(text, model) // Default model is gpt-3.5-turbo-16k-0613
 console.log(ai)
 })
 ```
--- index.js
+++ index.js
@@ -11,8 +11,54 @@ const listModel = [
   "gpt-3.5-turbo-16k-poe",
 ];
 
+const listModelV2 = [
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-0613",
+  "gpt-3.5-turbo-16k",
+  "gpt-3.5-turbo-16k-0613", // Default
+  "gpt-4",
+];
+
 async function model() {
   return listModel;
+}
+
+async function modelV2() {
+  return listModelV2;
+}
+
+async function generateV2(text, model) {
+  const getModel = model ? model : "gpt-3.5-turbo-16k-0613";
+  const { data } = await axios(
+    `https://freegpt-webui.ramonvc.repl.co/backend-api/v2/conversation`,
+    {
+      method: "post",
+      data: {
+        conversation_id: "a400053c-86ae-4001-8c10-1897752ca26",
+        action: "_ask",
+        model: getModel,
+        jailbreak: "default",
+        meta: {
+          id: "7258168646932333518",
+          content: {
+            conversation: [],
+            internet_access: false,
+            content_type: "text",
+            parts: [{ content: text, role: "user" }],
+          },
+        },
+      },
+      headers: {
+        accept: "text/event-stream",
+        "content-type": "application/json",
+      },
+    }
+  );
+  const result = {
+    model: getModel,
+    data: data,
+  };
+  return result;
 }
 
 async function generate(text, model, apikey) {
@@ -51,4 +97,4 @@ async function generate(text, model, apikey) {
   return result;
 }
 
-module.exports = { model, generate };
+module.exports = { model, modelV2, generateV2, generate };
