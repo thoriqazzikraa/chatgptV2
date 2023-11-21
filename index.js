e49295fe5b3b7df28769db9eedc4bb17aa2c1b92
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-11-21 12:29:12 +0700

    update nonce

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 async function generate(text) {
   try {
-    const params = new URLSearchParams({ _wpnonce: "7d6d48a588", post_id: 22, url: "https://chatgptt.me", action: "wpaicg_chat_shortcode_message", message: text, bot_id: 0 })
+    const params = new URLSearchParams({ _wpnonce: "55790ca9dd", post_id: 22, url: "https://chatgptt.me", action: "wpaicg_chat_shortcode_message", message: text, bot_id: 0 })
     const { data } = await axios.post(`https://chatgptt.me/wp-admin/admin-ajax.php`, params)
     return data
   } catch (err) {
