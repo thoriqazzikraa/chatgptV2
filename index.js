a573eae74b5310b0efeca092cccd2e2165d5741a
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-11-21 12:43:06 +0700

    sorry

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function generate(text) {
     const nonce = $("div.elementor-widget-wrap > div:nth-child(7) > div > div > div").attr("data-nonce")
     const params = new URLSearchParams({ _wpnonce: nonce, post_id: 22, url: "https://chatgptt.me", action: "wpaicg_chat_shortcode_message", message: text, bot_id: 0 })
     const dataa = await axios.post(`https://chatgptt.me/wp-admin/admin-ajax.php`, params)
-    return dataa
+    return dataa.data
   } catch (err) {
     console.log(err.response.data)
     return err.response.data.message
